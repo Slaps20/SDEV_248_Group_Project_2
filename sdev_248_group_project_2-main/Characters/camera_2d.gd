d84f62a671e348217f9a03eac56ed1b28a3f4591
HellerDeveller <150496986+HellerDeveller@users.noreply.github.com>
2025-05-02 00:18:52 -0400

    oopsie

--- sdev_248_group_project_2-main/Characters/camera_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D # we don't really need this
-
-func _process(delta: float) -> void:
-	var knight_pos = %"little_knight".position
-
-	self.position = knight_pos
--- sdev_248_group_project_2-main/Characters/global.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var playerDamageZone: Area2D
-
-var enemyDamageZone: Area2D
